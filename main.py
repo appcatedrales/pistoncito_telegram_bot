b973281e6bc62466ae4a8e837eb4cfe13e11314a
appcatedrales <argarcia90@gmail.com>
2025-04-04 17:17:03 +0200

    bueno

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import telegram
+from flask import Flask
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = telegram.Bot(token=TOKEN)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Pistoncito está online 🚀"
+
+@app.route("/mensaje_prueba")
+def enviar_mensaje():
+    bot.send_message(chat_id=CHAT_ID, text="✅ Pistoncito dice: estoy funcionando y puedo enviarte mensajes.")
+    return "Mensaje enviado"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
